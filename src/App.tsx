a2a02ffabee3dc42bf9b4dca42357d61e72f2650
Begon <japgo@naver.com>
2023-10-17 09:17:15 +0900

    예제 테스트 중.

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,146 @@
-import React, { ReactPropTypes } from 'react';
-import logo from './logo.svg';
+
+//import logo from './logo.svg';
 import './App.css';
-import { Context } from 'vm';
+import {useState} from 'react';
 
 interface header_props {
   title: string;
+  onChangeMode(): any;
+}
+
+interface article_props {
+  title: string;
+  body: string;
+}
+
+interface topic {
+  id: number;
+  title: string;
+  body: string;
+}
+interface nav_props {
+  topics: topic[];
+  onChangeMode( id: number ): any;
 }
 
 function Header( props: header_props ) {
   return (
       <header>
         <h1>
-          <a href="/">{props.title}</a>
+          <a href="/" onClick={ (event) => {
+            event.preventDefault();
+            props.onChangeMode();
+          }}>
+            {props.title}
+          </a>
         </h1>
       </header>
   )
 }
 
-function Nav() {
+function Nav( props: nav_props) {
+
+  const lis = [];
+  for( let i = 0; i < props.topics.length; i++ ) {
+    let t = props.topics[ i ];
+    lis.push( 
+      <li key={t.id}>
+        <a href={'/read/' + t.id} onClick={ event => {
+          event.preventDefault();
+          props.onChangeMode( t.id );
+        }}>
+          {t.title}
+        </a>
+      </li> 
+    );
+  }
+
   return (
       <nav>
         <ol>
-          <li><a href="/read/1">html</a></li>
-          <li><a href="/read/2">css</a></li>
-          <li><a href="/read/3">js</a></li>
-          <li><a href="/read/4">ts</a></li>
+          {lis}
         </ol>
       </nav>
   )
 }
 
-function Article() {
+function Article( props:article_props ) {
   return (
       <article>
-        <h2>Welcome</h2>
-        Hello, WEB
+        <h2>{props.title}</h2>
+        {props.body}
       </article>
   )
 }
 
-export default function MyApp() {
+function Create() {
+  return (
+    <article>
+      <h2>Create</h2>
+      <form onSubmit={event=>{
+        event.preventDefault();
+        const target = event.target as typeof event.target & {
+          title: { value: string };
+          body: { value: string };
+        }
+        const title = target.title;
+        const body = target.body;
+      }}>
+        <p><input type="text" name="title" placeholder="title" /></p>
+        <p><textarea name="body" placeholder="body"></textarea></p>
+        <p><input type="submit" value="Create"></input></p>
+      </form>
+    </article>
+  )
+}
+
+export default function App() {
+
+  const [mode, setMode] = useState( 'WELCOME' );
+  const [id, setId] = useState( 0 );
+
+  const topics = [
+    {id:1, title:'html', body:'html is ...'},
+    {id:2, title:'css', body:'css is ...'},
+    {id:3, title:'js', body:'js is ...'},
+  ];
+
+  let content = null;
+  if( mode === 'WELCOME' ) {
+    content = <Article title="Welcome" body="Hello, World!"></Article>
+  }
+  else if( mode === 'READ' ) {
+    let title = "", body = "";
+    for( let i = 0; i < topics.length; i++ ) {
+      if( topics[ i ].id === id ) {
+        title = topics[ i ].title;
+        body = topics[ i ].body;
+      }
+    }
+    content = <Article title={title} body={body}></Article>
+  }
+  else if( mode === 'CREATE' ) {
+    content = <Create onCreate={(title, body) => {
+
+    }}></Create>
+  }
+
   return (
     <div>
-      <Header title="REACT"></Header>
-      <Nav></Nav>
-      <Article></Article>
+      <Header title="WEB" onChangeMode={() => {
+        setMode( 'WELCOME' );
+      }}></Header>
+      <Nav topics={topics} onChangeMode={(id) => {
+        setMode( 'READ' );
+        setId( id );
+      }}></Nav>
+      { content }
+      <a href="/create" onClick={event=>{
+        event.preventDefault();
+        setMode( 'CREATE' );
+      }}>
+        Create
+      </a>
     </div>
   )
 }
